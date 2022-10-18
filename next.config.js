6744d20598b289cceca974dc3950d3d9e29e5685
Ednan <ednanjhony@yahoo.com.br>
2022-10-18 06:48:49 -0300

    doing navigation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ]
+  },
+
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
 }
 
 module.exports = nextConfig
